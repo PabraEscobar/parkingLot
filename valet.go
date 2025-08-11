55982e4608d9465eb194f5906619197570ccd8a3
AshokPabra <ashokpabra1858@gmail.com>
2025-08-11 16:38:42 +0530

    Make Attendant exported

--- valet.go
+++ valet.go
@@ -4,39 +4,39 @@ import (
 	"errors"
 )
 
-type attendant struct {
+type Attendant struct {
 	lot         *Lot
 	parkingFull bool
 }
 
 // attendant implements ParkingFullReceiver
-func (a *attendant) ParkingFullReceive() {
+func (a *Attendant) ParkingFullReceive() {
 	a.parkingFull = true
 }
 
 // attendant implements ParkingStatusReceiver
-func (a *attendant) ParkingStatusReceive(status ParkingStatus) {
+func (a *Attendant) ParkingStatusReceive(status ParkingStatus) {
 	if status == ParkingAvailable {
 		a.parkingFull = false
 	}
 }
 
-func (a *attendant) Unpark(vehicleNumber string) (*vehicle, error) {
+func (a *Attendant) Unpark(vehicleNumber string) (*vehicle, error) {
 	return a.lot.Unpark(vehicleNumber)
 }
 
-func (a *attendant) Park(vehicleNumber string) (*vehicle, error) {
+func (a *Attendant) Park(vehicleNumber string) (*vehicle, error) {
 	if a.parkingFull {
 		return nil, errors.New("parking is full")
 	}
 	return a.lot.Park(vehicleNumber)
 }
 
-func NewAttendant(lot *Lot) (*attendant, error) {
+func NewAttendant(lot *Lot) (*Attendant, error) {
 	if lot == nil {
 		return nil, errors.New("attendant does not exist without parking lot")
 	}
-	a := &attendant{lot: lot}
+	a := &Attendant{lot: lot}
 	lot.SubscribeParkingFullStatus(a)
 	lot.subscriberParkingStatus = a
 	return a, nil
