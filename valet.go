d5196a7a638d9c506bc03ded42cc32f5b9bc29a4
AshokPabra <ashokpabra1858@gmail.com>
2025-08-12 14:21:54 +0530

    make type attendant unexported

--- valet.go
+++ valet.go
@@ -4,35 +4,35 @@ import (
 	"errors"
 )
 
-type Attendant struct {
+type attendant struct {
 	lot         *Lot
 	parkingFull bool
 }
 
 // attendant implements ParkingFullReceiver
-func (a *Attendant) ParkingFullReceive() {
+func (a *attendant) ParkingFullReceive() {
 	a.parkingFull = true
 }
 
-func (a *Attendant) Unpark(vehicleNumber string) (*vehicle, error) {
-	if a.parkingFull{
-		a.parkingFull=false
+func (a *attendant) Unpark(vehicleNumber string) (*vehicle, error) {
+	if a.parkingFull {
+		a.parkingFull = false
 	}
 	return a.lot.Unpark(vehicleNumber)
 }
 
-func (a *Attendant) Park(vehicleNumber string) (*vehicle, error) {
+func (a *attendant) Park(vehicleNumber string) (*vehicle, error) {
 	if a.parkingFull {
 		return nil, errors.New("parking is full")
 	}
 	return a.lot.Park(vehicleNumber)
 }
 
-func NewAttendant(lot *Lot) (*Attendant, error) {
+func NewAttendant(lot *Lot) (*attendant, error) {
 	if lot == nil {
 		return nil, errors.New("attendant does not exist without parking lot")
 	}
-	a := &Attendant{lot: lot}
+	a := &attendant{lot: lot}
 	lot.SubscribeParkingFullStatus(a)
 	return a, nil
 }
